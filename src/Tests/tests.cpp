d60aacd9704b9c909ac63c3b0b13de60e05d024b
Curran Carr <curranca@et-d5.kzn.21-school.ru>
2022-10-29 14:38:21 +0300

    Added DFS tests

--- src/Tests/tests.cpp
+++ src/Tests/tests.cpp
@@ -14,6 +14,21 @@ TEST(PartOneTests, DFS) {
     for (int i = 0; i < 5; i++) {
         EXPECT_EQ(result[i], expected_values[i]);
     }
+
+    result = graph_algorithms.DepthFirstSearch(graph, 999);
+    EXPECT_EQ(result.at(0), -1);
+
+    Graph empty_graph;
+    result = graph_algorithms.DepthFirstSearch(empty_graph, 2);
+    EXPECT_EQ(result.at(0), -2);
+
+    graph.GetMatrixFromFile("DotFiles/DFS2.dot");
+    result = graph_algorithms.DepthFirstSearch(graph, 3);
+    int expected_values2[] = {3, 1, 2, 4};
+    EXPECT_EQ(result.size(), 4);
+    for (int i = 0; i < 4; i++) {
+        EXPECT_EQ(result[i], expected_values2[i]);
+    }
 }
 
 
