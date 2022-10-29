84a805be47f560e0581d519fc302d943240d0840
Pilaf Berenice <pilafber@et-d1.kzn.21-school.ru>
2022-10-29 04:16:28 +0300

    a

--- src/GraphAlgorithms/GraphAlgorithms.cpp
+++ src/GraphAlgorithms/GraphAlgorithms.cpp
@@ -1,6 +1,5 @@
 #include "GraphAlgorithms.h"
 
-
 namespace s21 {
 
 std::vector<int> GraphAlgorithms::DepthFirstSearch(Graph &graph, int start_vertex) {
@@ -8,7 +7,7 @@ std::vector<int> GraphAlgorithms::DepthFirstSearch(Graph &graph, int start_verte
     if (graph.GetMatrix().get_cols() == 0) {
         result.push_back(EMPTY_GRAPH_ERROR);
     }
-    S21Matrix& matrix = graph.GetMatrix();
+    S21Matrix &matrix = graph.GetMatrix();
     if (start_vertex > matrix.get_cols() || start_vertex <= 0) {
         result.push_back(WRONG_VERTEX_NUMBER);
         return result;
@@ -23,9 +22,9 @@ std::vector<int> GraphAlgorithms::DepthFirstSearch(Graph &graph, int start_verte
         result.push_back(current_vertex);
         visited.insert(current_vertex);
 
-//        debug_print_vector(result);
-//        debug_print_stack(stack);
-//        debug_print_set(visited);
+        //        debug_print_vector(result);
+        //        debug_print_stack(stack);
+        //        debug_print_set(visited);
 
         for (int i = 0; i < matrix.get_cols(); i++) {
             if (matrix(current_vertex - 1, i) && visited.find(i + 1) == visited.end()) {
@@ -62,7 +61,53 @@ void GraphAlgorithms::debug_print_set(std::unordered_set<int> set) {
 }
 
 int GraphAlgorithms::GetShortestPathBetweenVertices(Graph &graph, int vertex1, int vertex2) {
+    std::vector<int> pos, node, parent;
+    int big_number = std::numeric_limits<int>::max();
+    int size = graph.GetMatrix().get_rows();
+    pos.resize(size);
+    node.resize(size);
+    parent.resize(size);
+    for (int i = 0; i < size; ++i) {
+        pos[i] = big_number;
+        node[i] = 0;
+        parent[i] = -1;
+    }
 
+    int min = 0, index_min = 0;
+    pos[vertex1 - 1] = 0;
+    for (int i = 0; i < size; ++i) {
+        min = big_number;
+        for (int j = 0; j < size; ++j) {
+            if (!node[j] && pos[j] < min) {
+                min = pos[j];
+                index_min = j;
+            }
+        }
+        node[index_min] = 1;
+        for (int j = 0; j < size; ++j) {
+            if (!node[j] && graph.GetMatrix()(index_min, j) && pos[index_min] != big_number &&
+                pos[index_min] + graph.GetMatrix()(index_min, j) < pos[j]) {
+                pos[j] = pos[index_min] + graph.GetMatrix()(index_min, j);
+                parent[j] = index_min;
+            }
+        }
+    }
+
+    // вывод пути
+    // std::vector<int> temp;
+    // for (int i = vertex2 - 1; i != -1; i = parent[i]) temp.push_back(i + 1);
+    // std::reverse(temp.begin(), temp.end());
+    // for (int i = 0; i < temp.size(); ++i) std::cout << temp[i] << " ";
+    // std::cout << std::endl;
+    return pos[vertex2 - 1];
 }
 
 }  // namespace s21
+
+// int main() {
+//     s21::Graph graph;
+//     graph.GetMatrixFromFile("/Users/pilafber/Projects/2/src/DotFiles/graph");
+//     s21::GraphAlgorithms graph_algs;
+//     graph_algs.GetShortestPathBetweenVertices(graph, 1, 4);
+//     return 0;
+// }
--- src/GraphAlgorithms/GraphAlgorithms.h
+++ src/GraphAlgorithms/GraphAlgorithms.h
@@ -1,11 +1,14 @@
 #ifndef A2_SIMPLENAVIGATOR_V1_0_0_MASTER_GRAPHALGORITHMS_H
 #define A2_SIMPLENAVIGATOR_V1_0_0_MASTER_GRAPHALGORITHMS_H
 
-#include <vector>
+#include <limits>
 #include <unordered_set>
-#include "../Graph/Graph.h"
-#include "../DataStructures/Stack/Stack.h"
+#include <vector>
+#include <algorithm>
+
 #include "../DataStructures/Queue/Queue.h"
+#include "../DataStructures/Stack/Stack.h"
+#include "../Graph/Graph.h"
 
 namespace s21 {
 
@@ -14,13 +17,10 @@ struct TsmResult {
     double distance;
 };
 
-enum Status {
-    WRONG_VERTEX_NUMBER = -1,
-    EMPTY_GRAPH_ERROR = -2
-};
+enum Status { WRONG_VERTEX_NUMBER = -1, EMPTY_GRAPH_ERROR = -2 };
 
 class GraphAlgorithms {
- public:
+public:
     std::vector<int> DepthFirstSearch(Graph &graph, int start_vertex);
     std::vector<int> BreadthFirstSearch(Graph &graph, int startVertex);
 
@@ -31,11 +31,11 @@ class GraphAlgorithms {
 
     TsmResult SolveTravelingSalesmanProblem(Graph &graph);
 
- private:
+private:
     void debug_print_vector(std::vector<int> vector);
     void debug_print_stack(Stack stack);
     void debug_print_set(std::unordered_set<int> set);
 };
 }  // namespace s21
 
-#endif //A2_SIMPLENAVIGATOR_V1_0_0_MASTER_GRAPHALGORITHMS_H
+#endif  // A2_SIMPLENAVIGATOR_V1_0_0_MASTER_GRAPHALGORITHMS_H
